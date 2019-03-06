6a8e549331c19f599bc4be68145b15d57bbf6b36
Brandon <Blent1050@yahoo.com>
2019-03-06 16:04:23 -0500

    added tests to controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,42 @@
-// Test away!
+import React from 'react';
+
+//Component Import
+import Controls from './Controls';
+
+//Testing imports
+import renderer from 'react-test-renderer';
+import {render, fireEvent} from 'react-testing-library';
+
+//Testing Execution Imports
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+describe('<Controls />', () => {
+  //Run tests on the toggleLocked() function
+  describe('toggleClosed()', () => {
+    it('should call the toggleLocked function', () => {
+      const toggleClosed = jest.fn();
+      const {getByText} = render(<Controls toggleClosed={toggleClosed} />);
+
+      fireEvent.click(getByText(/close gate/i));
+      expect(toggleClosed).toHaveBeenCalled();
+    });
+    it('should toggle the text of the button from closed gate to open gate', () => {
+      const toggleClosed = jest.fn(() => {
+        this.setState(prev => ({closed: !prev.closed}));
+      });
+      const {getByText} = render(<Controls toggleClosed={toggleClosed} />);
+
+      expect(getByText(/close gate/i).textContent).toBe('Close Gate');
+      fireEvent.click(getByText(/close gate/i));
+      //   expect(getByText(/open gate/i).textContent).toBe('Open Gate');
+    });
+  });
+
+  //Snapshots
+  it('Matches Snapshot', () => {
+    const tree = renderer.create(<Controls />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,2 +1,20 @@
-// Test away
-//tests
+import React from 'react';
+
+//Component to test
+import Dashboard from './Dashboard';
+
+//Testing Libraries
+import {render, fireEvent} from 'react-testing-library';
+import renderer from 'react-test-renderer'; //Snapshot Library
+//Execution Testing Libraries
+import 'react-testing-library/cleanup-after-each'; //Resets the DOM after each test
+import 'jest-dom/extend-expect';
+
+describe('<Dashboard />', () => {
+  //Snapshot
+  it.skip('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
